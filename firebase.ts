b51a13d1b747e6af12aec14dd4a7ee0a3265ea74
dilshanbuddhi <dilshanbuddhi40@gmail.com>
2025-08-29 11:07:35 +0530

    firebase storage

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8UeThj2zlSi5l-M0WGen_BU4bmZbiTl8",
-  authDomain: "task-manager-app-ca147.firebaseapp.com",
-  projectId: "task-manager-app-ca147",
-  storageBucket: "task-manager-app-ca147.firebasestorage.app",
-  messagingSenderId: "3155864725",
-  appId: "1:3155864725:web:cae6728b9b4376ed94bf9c"
-}
+  apiKey: "AIzaSyD06lG6LCkv2nzC2NHxRaDOpTv6bLIg-aM",
+  authDomain: "task-manager-155b5.firebaseapp.com",
+  projectId: "task-manager-155b5",
+  storageBucket: "task-manager-155b5.firebasestorage.app",
+  messagingSenderId: "309278626612",
+  appId: "1:309278626612:web:bb5813be75f05cdc8e0b61"
+};
 
 const app = initializeApp(firebaseConfig)
 
