0711b722f78f8bb2d7b5f6c3e359856cf27ead4e
dilshanbuddhi <dilshanbuddhi40@gmail.com>
2025-09-22 10:52:47 +0530

    build setup

--- app.config.js
+++ app.config.js
@@ -1,7 +1,13 @@
 export default {
   name: "cine_track",
   slug: "cine_track",
+  android: {
+    package: "com.dilshan.cinetrack"
+  },
   extra: {
-    mockApi: process.env.EXPO_BASE_API_URL
+    mockApi: process.env.EXPO_BASE_API_URL,
+    eas: {
+      projectId: "742b7ba9-e14c-45cb-9d9b-517883657404"
+    }
   }
 }
