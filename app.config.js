ab0c3653cd1962234718e52318a64b847f227ccf
dilshanbuddhi <dilshanbuddhi40@gmail.com>
2025-09-30 10:54:43 +0530

    Update code for new build

--- app.config.js
+++ app.config.js
@@ -4,10 +4,13 @@ export default {
   android: {
     package: "com.dilshan.cinetrack"
   },
+  plugins: [
+    "expo-router"
+  ],
   extra: {
     mockApi: process.env.EXPO_BASE_API_URL,
     eas: {
       projectId: "742b7ba9-e14c-45cb-9d9b-517883657404"
     }
   }
-}
+};
